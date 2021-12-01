6603ad525cba3bda4bfae2b29d88f6cb356c834a
Matias Mercado 02 <matymercadoone@gmail.com>
2021-12-01 03:36:33 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          tienda e-commerce por Matias Mercado
         </a>
       </header>
     </div>
