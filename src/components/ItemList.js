c25f37b8203e7a35c03206f31d9613398aa9c947
Matias Mercado 02 <matymercadoone@gmail.com>
2021-12-15 19:58:51 -0300

    eliminacion de comentarios innecesarios

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -20,14 +20,12 @@ export default function  ItemList() {
                 
                 return valorActual.id 
                 ? 
-                //aqui le hago una pregunta, es obvio que puse la misma imagen para todas las fotos, y es por que cuando intentaba hacerlo con las propias no me dejaba, src={valorActual.img} eso es lo que habia intentado antes.
                 
                 <div style={productStyle} key={ valorActual.id + "b"}>
 
                         <div>{valorActual.name}</div>
                             
                         <img height={150} src={valorActual.img}/>
-                        
                         
                         <p>Precio: (Usd)<b> {valorActual.price}</b></p>
 
@@ -44,6 +42,3 @@ const productStyle = {
     margin : "5px",
     display : "inline-block"
 }
-
-
-// <img height={150} src="https://st.depositphotos.com/1001555/2719/i/600/depositphotos_27190451-stock-photo-blank-computer-monitor.jpg"/>
