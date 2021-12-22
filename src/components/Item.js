ab47d0e0f6908dbaddc7f657bae34de7eac6870d
Matias Mercado 02 <matymercadoone@gmail.com>
2021-12-22 20:58:57 -0300

    cambios en la estructura realizados

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,23 +1,28 @@
-import ImgProduct1 from './Img/producto1.jpg';
-import ImgProduct2 from './Img/producto2.jpg';
-import ImgProduct3 from './Img/producto3.jpg';
-import ImgProduct4 from './Img/producto4.jpg';
-import ImgProduct5 from './Img/producto5.jpg';
-import ImgProduct6 from './Img/producto6.jpg';
-import ImgProduct7 from './Img/producto7.jpg';
-import ImgProduct8 from './Img/producto8.jpg';
-import ImgProduct9 from './Img/producto9.jpg';
+const Item = ({ valorActual }) => {
 
-const array = [
-    {id : 1, name : "Lenovo", img : ImgProduct1, price : 111   },
-    {id : 2, name : "Mac", img : ImgProduct2, price : 3012   },
-    {id : 3, name : "Notebook 3 ", img : ImgProduct3, price : 532   },
-    {id : 4, name : "Lenovo ThinkPad", img : ImgProduct4, price : 954   },
-    {id : 5, name : "Arius", img : ImgProduct5, price : 441   },
-    {id : 6, name : "Omega 3", img : ImgProduct6, price : 652   },
-    {id : 7, name : "Potasio", img : ImgProduct7, price : 1928   },
-    {id : 8, name : "Render", img : ImgProduct8, price : 901   },
-    {id : 9, name : "object Object", img : ImgProduct9, price : 301   }
-]
+    return (
 
-export default array
+                valorActual.id 
+                ? 
+                
+                <div style={productStyle} key={ valorActual.id + "b"}>
+
+                        <div>{valorActual.name}</div>
+                            
+                        <img height={150} src={valorActual.img}/>
+                        
+                        <p>Precio: (Usd)<b> {valorActual.price}</b></p>
+
+                </div>
+                
+                
+                : <div></div>
+
+    )
+}
+export default Item
+const productStyle = {
+    margin : "5px",
+    display : "inline-block"
+}
+
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,34 +1,17 @@
-
+import Item from './Item';
 // ITEMLISTCOINTAINER ME PASA COMO PROP EL STATE Y LO MAPEO, LUEGO LO RENDERIZO EN APP
 
 export default function  ItemList({state}) {
-    // const [state, setState] = useState([])
 
     return (
+        <>
+        
         <div>
-            {state.map((valorActual)=>{
-                
-                return valorActual.id 
-                ? 
-                
-                <div style={productStyle} key={ valorActual.id + "b"}>
 
-                        <div>{valorActual.name}</div>
-                            
-                        <img height={150} src={valorActual.img}/>
-                        
-                        <p>Precio: (Usd)<b> {valorActual.price}</b></p>
-
-                </div>
-                
-                
-                : <div></div>
-            })}
+            {state.map(valorActual => <Item key={valorActual.id} 
+                valorActual={valorActual} />)}
+            
         </div>
+        </> 
     )
-}
-
-const productStyle = {
-    margin : "5px",
-    display : "inline-block"
 }
